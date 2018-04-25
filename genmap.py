e465139b542db9af172e170ab0f771ab46cfe9e9
Michał Radecki <mr371591@students.mimuw.edu.pl>
2018-04-25 19:43:23 +0200

    Dodany generator mapy

--- genmap.py
+++ genmap.py
@@ -1,2 +1,80 @@
 import random
-ruch_pion = []
+
+def daj_plansze():
+	plansza = []
+	for x in range(48):
+		kolumna = []
+		for y in range(27):
+			kolumna.append({"znak": '.'})
+		plansza.append(kolumna)
+
+	# print(plansza[44][20])
+
+	pozycje_przeciwnikow = []
+	for i in range(10):
+		udalo_sie = False
+		ile_razy_prubowalem = 0
+		while udalo_sie == False and ile_razy_prubowalem < 10:
+			udalo_sie = True
+			x = random.randint(1, 46)
+			y = random.randint(1, 25)
+			for pos_x, pos_y in pozycje_przeciwnikow:
+				dif_x = pos_x - x
+				dif_y = pos_y - y
+				if(not (dif_x <= -10 or dif_x >= 10 or dif_y <= -10 or dif_y >= 10)):
+					udalo_sie = False
+			ile_razy_prubowalem += 1
+
+		if ile_razy_prubowalem < 10:
+			pozycje_przeciwnikow.append((x, y))
+			plansza[x][y]["znak"] = 'P'
+
+	# print("plansza1")
+	# for y in range(27):
+	# 	line = ""
+	# 	for x in range(48):
+	# 		line += plansza[x][y]["znak"]
+	# 		line += plansza[x][y]["znak"]
+	# 	print(line)
+	# 	print(line)
+
+
+	# print("plansza2")
+	for los in range(20):
+		pos1 = pozycje_przeciwnikow[random.randint(0, len(pozycje_przeciwnikow) - 1)]
+		pos2 = pozycje_przeciwnikow[random.randint(0, len(pozycje_przeciwnikow) - 1)]
+		if pos1[0] < pos2[0]:
+			l = pos1[0]
+			r = pos2[0]
+		else:
+			l = pos2[0]
+			r = pos1[0]
+
+		if pos1[1] < pos2[1]:
+			u = pos1[1]
+			d = pos2[1]
+		else:
+			u = pos2[1]
+			d = pos1[1]
+		for x in range(l, r+1):
+			if plansza[x][u]["znak"] != "P":
+				plansza[x][u]["znak"] = "S"
+		for y in range(u, d+1):
+			if (
+				(pos1[0] == l and pos1[1] == u and pos2[0] == r and pos2[1] == d)
+				or (pos2[0] == l and pos2[1] == u and pos1[0] == r and pos1[1] == d)
+			):
+				if plansza[r][y]["znak"] != "P":
+					plansza[r][y]["znak"] = "S"
+			else:
+				if plansza[l][y]["znak"] != "P":
+					plansza[l][y]["znak"] = "S"
+	return {"plansza": plansza, "enemies_pos": pozycje_przeciwnikow}
+
+	# for y in range(27):
+	# 	line = ""
+	# 	for x in range(48):
+	# 		line += plansza[x][y]["znak"]
+	# 		line += plansza[x][y]["znak"]
+	# 	print(line)
+	# 	print(line)
--- objects.py
+++ objects.py
@@ -1,6 +1,7 @@
 import pygame
 import os.path
 from colors import Colors
+from genmap import daj_plansze
 
 class Monster(object):
 	def __init__(self, x, y):
@@ -42,38 +43,48 @@ class Map(object):
 				} for y in range(27)
 			] for x in range(48)
 		]
+		dane_o_planszy = daj_plansze()
 
-		e1 = [(6, 2), (40, 2), (6, 23), (40, 23)]
-		e2 = [(17, 3), (29, 3), (17, 22), (29, 22), (4, 12), (42, 12)]
-		enemies_positions = e1 + e2
-		self.monsters = [Monster(pos_x, pos_y) for pos_x, pos_y in enemies_positions]
-
+		# e1 = [(6, 2), (40, 2), (6, 23), (40, 23)]
+		# e2 = [(17, 3), (29, 3), (17, 22), (29, 22), (4, 12), (42, 12)]
+		# enemies_positions = e1 + e2
+		self.monsters = [Monster(pos_x, pos_y) for pos_x, pos_y in dane_o_planszy["enemies_pos"]]
+		#
 		t1 = [(23 - 3.625, 10), (27, 10), (23 - 3.625, 14), (27, 14)]
 		trees_positions = t1
 		self.trees = [Tree(pos_x, pos_y) for pos_x, pos_y in trees_positions]
 
-		# dodaje przeciwnikow
-		for pos_x, pos_y in enemies_positions:
-			for x in range(2):
-				for y in range(2):
-					self.map[pos_x + x][pos_y + y]["color"] = Colors.MONSTER
-					self.map[pos_x + x][pos_y + y]["solid"] = True
-
+		#
 		#dodaje kamienie
 		# pionowe
-		for y in range(4, 23): # bez 24
-			for x in [6, 23, 40]:
-				self.map[x][y]["color"] = Colors.STONE
-				self.map[x + 1][y]["color"] = Colors.STONE
-		#poziome
-		for x in range(6, 42): # bez 42
-			self.map[x][12]["color"] = Colors.STONE
-			self.map[x][13]["color"] = Colors.STONE
-		for x in range(19, 29):
-			for y in [3, 22]:
-				self.map[x][y]["color"] = Colors.STONE
-				self.map[x][y + 1]["color"] = Colors.STONE
 
+		for x in range(47):
+			for y in range(26):
+				if dane_o_planszy["plansza"][x][y]["znak"] == "S":
+					for delta_x in range(2):
+						for delta_y in range(2):
+							self.map[x + delta_x][y + delta_y]["color"] = Colors.STONE
+
+		#
+		# dodaje przeciwnikow
+		for pos_x, pos_y in dane_o_planszy["enemies_pos"]:
+			for delta_x in range(2):
+				for delta_y in range(2):
+					self.map[pos_x + delta_x][pos_y + delta_y]["color"] = Colors.MONSTER
+					self.map[pos_x + delta_x][pos_y + delta_y]["solid"] = True
+			# for x in range(2):
+			# 	for y in range(2):
+			# 		self.map[pos_x + x][pos_y + y]["color"] = Colors.MONSTER
+			# 		self.map[pos_x + x][pos_y + y]["solid"] = True
+		# #poziome
+		# for x in range(6, 42): # bez 42
+		# 	self.map[x][12]["color"] = Colors.STONE
+		# 	self.map[x][13]["color"] = Colors.STONE
+		# for x in range(19, 29):
+		# 	for y in [3, 22]:
+		# 		self.map[x][y]["color"] = Colors.STONE
+		# 		self.map[x][y + 1]["color"] = Colors.STONE
+		#
 		#koniec planszy
 		for x in range(48):
 			for y in [0, 26]:
